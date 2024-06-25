97645b1c1679f7850a98cb46d5d6b33a76079a70
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-06-25 14:18:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-cat pip.txt > ~/.config/pip/pip.conf
-pip3 install selenium notebook --break-system-packages
-echo -e "e\ne" | jupyter notebook password 
-jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
+python hld.py & gunicorn app:app
